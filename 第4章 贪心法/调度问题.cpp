333036ba5158ece6cef9907bd61a7ad9e67777b1
Jinsane#5 <zhangjqfriend@126.com>
2019-11-29 20:47:59 +0800

    schedule

---
+++ 第4章 贪心法/调度问题.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Task{
+    int t;  //占用时间
+    int d;  //希望完成时间
+};
+
+vector<int> Schedule(vector<Task> tasks);
+bool cmp(Task a, Task b);
+
+int main()
+{
+    vector<Task> tasks = {{5,10},{8,12},{4,15},{10,11},{3,20}};
+    vector<int> A = Schedule(tasks);
+    cout << "schedule(start time) is:" << endl;
+    for (vector<int>::iterator it=A.begin(); it!=A.end(); ++it){
+        cout << *it << " ";
+    }
+    cout << endl;
+    return 0;
+}
+bool cmp(Task a, Task b){
+    return a.d < b.d;
+}
+
+//算法4.3 调度问题，给定一串任务的服务时间t和希望完成时间d，延迟为服务结束时间减di。
+//求调度f表示任务开始时间，使得最大延迟最小
+vector<int> Schedule(vector<Task> tasks){
+    int maxDelay = 0;
+    vector<int> A;
+    //按照希望完成时间排序即可
+    sort(tasks.begin(), tasks.end(), cmp); //需要include <algorithm>
+    A.push_back(0);
+    int curTime = tasks[0].t;
+    int curDelay = curTime - tasks[0].d;
+    if (curDelay>maxDelay){
+        maxDelay = curDelay;
+    }
+    for (vector<Task>::iterator it=tasks.begin()+1; it!=tasks.end(); ++it){
+            A.push_back(curTime);
+            curTime += it->t;
+            curDelay = curTime - it->d;
+            if (curDelay>maxDelay){
+                maxDelay = curDelay;
+            }
+    }
+    cout << "max delay is: \n" << maxDelay << endl;
+    return A;
+}
